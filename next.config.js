4ddd9439f8a3c777c551a9d8299949aec8177d0b
adelnizamutdinov <stiggpwnz@gmail.com>
2021-03-31 10:58:54 +0300

    modernize

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && isServer) {
+      require('./scripts/nextgen')
+    }
+
+    return config
+  }
+}
